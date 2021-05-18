d4723263bc496232f2773b423559922ba9e39ebc
dixyes <dixyes@gmail.com>
2021-05-18 14:20:31 +0800

    use seperate main logic

--- phpbc.php
+++ phpbc.php
@@ -1,6 +1,12 @@
 <?php
 
-// PHPbc - phpbc.php main program
+declare(strict_types=1);
 
+include_once __DIR__ . "/vendor/autoload.php";
 
+use PHPbc\PHPbc;
+use PHPbc\Util;
 
+// make all warnings into exceptions
+Util::enable_error_handler();
+PHPbc::run();
